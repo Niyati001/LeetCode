67bcee1aeea1d78203dd8a21625d2df9d2d05e46
Niyati <ngupta6_be23@thapar.edu>
2026-03-24 10:28:42 +0530

    Time: 6 ms (23.81%) | Memory: 9.8 MB (23.71%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> count1(26,0);
+        vector<int> count2(26,0);
+
+        for(char c: s) count1[c- 'a']++;
+        for(char c: t) count2[c- 'a']++;
+
+        if(count1== count2) return true;
+        else return false;
+    }
+};
+
+//for (int i = 0; i < s.length(); i++) {
+//    char c = s[i];
+//    count1[c - 'a']++;
+//}
