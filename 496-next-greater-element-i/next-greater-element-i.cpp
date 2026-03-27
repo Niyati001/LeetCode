5e748af29ee92681ca6def65c7609431a73d6b56
Niyati <ngupta6_be23@thapar.edu>
2026-03-27 20:40:14 +0530

    Time: 6 ms (12.42%) | Memory: 12.9 MB (13.60%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mp;
+        vector<int> arr;
+
+        for(int i=0; i<nums2.size(); i++){
+            while(!st.empty() && st.top()< nums2[i]){
+                mp[st.top()]= nums2[i];
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+            while(!st.empty()){
+                mp[st.top()]= -1;
+                st.pop();
+            }
+            for(int i: nums1){
+                arr.push_back(mp[i]);
+            }
+        return arr;
+    }
+};
