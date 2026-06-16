7de02aa0f6401a61bc069c5a3b362f7880c86160
Niyati <ngupta6_be23@thapar.edu>
2026-06-16 17:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.51%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N=n;
+        long long N = n;  // handle INT_MIN safely
 
-        if(N<0){
-            x= 1/x;
-            N= -N;
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
         }
-        double ans= 1.0;
+
+        double ans = 1.0;
+
         while(N>0){
-            if(N%2==1){
-                ans= ans*x;
-                N--;
-            }
-            else{
-                x= x*x;
-                N= N/2;
-            }
+            if(N&1)
+                ans*= x;
+            x*=x;
+            N>>= 1;
         }
         return ans;
     }
