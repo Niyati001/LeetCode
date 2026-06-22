4b954096fcc141fb78fd40807b55189a03c1a70d
Niyati <ngupta6_be23@thapar.edu>
2026-06-22 19:56:25 +0530

    Time: 4 ms (22.37%) | Memory: 16.5 MB (32.10%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,33 +12,37 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        if(!root) return {};
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0,0}});
+        map<int, map<int, multiset<int>>>mp;
+        queue<pair<TreeNode*, pair<int, int>>>q;
 
-        while(!todo.empty()){
-            auto p= todo.front();
-            todo.pop();
-            TreeNode* node= p.first;
-            int x= p.second. first;
-            int y= p.second.second;
-            nodes[x][y].insert(node-> val);
-            if(node-> left){
-                todo.push({node->left, {x-1, y+1}});
-            }
-            if(node-> right){
-                todo.push({node-> right, {x+1, y+1}});
-            }
+        q.push({root, {0,0}});
+
+        while(!q.empty()){
+            auto temp= q.front();
+            q.pop();
+
+            TreeNode* node= temp.first;
+            int x= temp.second.first; //vertical
+            int y= temp.second.second;  //level
+            
+            mp[x][y].insert(node-> val);
+
+            if(node-> left)
+                q.push({node->left, {x-1, y+1}});
+            
+            if(node-> right)
+                q.push({node-> right, {x+1, y+1}});
         }
+
         vector<vector<int>> ans;
-        for(auto p: nodes){
+
+        for(auto p: mp){
             vector<int> col;
             for(auto q: p.second){
                 col.insert(col.end(), q.second.begin(), q.second.end());
             }
             ans.push_back(col);
-        } 
+        }
         return ans;
     }
 };
