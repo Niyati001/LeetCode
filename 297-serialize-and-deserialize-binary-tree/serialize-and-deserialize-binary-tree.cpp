9ef86ea01445cac6b4efce3726d9aa2c528d321f
Niyati <ngupta6_be23@thapar.edu>
2026-06-23 15:51:38 +0530

    Time: 24 ms (76.24%) | Memory: 35.7 MB (38.84%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    void preOrder(TreeNode* root, string &s){
+        if(root==NULL){
+            s+= "#,";
+            return;
+        }
+        s+= to_string(root-> val)+ ",";
+
+        preOrder(root-> left, s);
+        preOrder(root-> right, s);
+    }
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string s;
+        preOrder(root, s);
+
+        return s;
+    }
+    
+    TreeNode* buildTree(stringstream &ss){
+        string val;
+        getline(ss, val , ',');
+
+        if(val== "#") return NULL;
+
+        TreeNode* root= new TreeNode(stoi(val));
+        root-> left= buildTree(ss);
+        root-> right= buildTree(ss);
+
+        return root;
+    }
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return buildTree(ss);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
