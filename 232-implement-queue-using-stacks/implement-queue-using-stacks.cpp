0358013e93d49ca6e7b860a8aa35113ff23894f6
Niyati <ngupta6_be23@thapar.edu>
2026-06-27 14:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (26.35%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,37 +1,36 @@
 class MyQueue {
-    stack<int> s1, s2;
 public:
-    MyQueue() {}
+    stack<int> s1, s2;
+    MyQueue() {
+        
+    }
     
     void push(int x) {
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
         s1.push(x);
+
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        if(s2.empty()){
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        int val= s2.top();
-        s2.pop();
-        return val;
+        int ans= s1.top();
+        s1.pop();
+        return ans;
     }
     
     int peek() {
-        if(s2.empty()){
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-            return s2.top();
+        return s1.top();
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
-        
+        if(s1.empty()) return true;
+        return false;
     }
 };
 
