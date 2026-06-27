ed1424d038988fd9e5474adfad437b3474fddb70
Niyati <ngupta6_be23@thapar.edu>
2026-06-27 15:50:52 +0530

    Time: 3 ms (9.35%) | Memory: 10.2 MB (6.60%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0; i<s.size(); i++){
-            char ch= s[i];
+        stack<int> st;
         
-        if(ch== '(' || ch== '{' || ch== '['){
-            st.push(ch);
-        }
-        else{
-            if(st.empty()) return false;
-
-            char top= st.top();
-            if(ch== ')' && top== '(' || ch== '}' && top== '{' || ch== ']' && top== '['){
+        for(char ch: s){
+            if(ch== '(') st.push(')');
+            else if(ch== '{') st.push('}');
+            else if(ch== '[') st.push(']');
+            else{
+                if(st.empty() || st.top()!= ch)
+                    return false;
                 st.pop();
             }
-            else{
-                return false;
-            }
-        }
         }
         return st.empty();
     }
