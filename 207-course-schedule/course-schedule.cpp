cf3ba2f69d832adbdec99c97c316ec9ebc04b0b8
Niyati <ngupta6_be23@thapar.edu>
2026-08-07 20:15:04 +0530

    Time: 6 ms (49.74%) | Memory: 20.1 MB (32.32%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int>inDegree(numCourses, 0);
+        vector<int> indegree(numCourses, 0);
 
         for(auto edge: prerequisites){
-            int u= edge[1];
-            int v= edge[0];
-            
+            int u= edge[0];
+            int v= edge[1];
+
             adj[u].push_back(v);
-            inDegree[v]++;
+            indegree[v]++;
         }
+
         queue<int> q;
 
-        for(int i=0; i<numCourses; i++){
-            if(inDegree[i]==0)
+        for(int i=0; i< numCourses; i++){
+            if(indegree[i]==0)
                 q.push(i);
         }
         int count=0;
-        
+
         while(!q.empty()){
             int node= q.front();
             q.pop();
 
             count++;
-            for(auto neigh: adj[node]){
-                inDegree[neigh]--;
 
-                if(inDegree[neigh]==0) q.push(neigh);
+            for(auto neigh: adj[node]){
+                indegree[neigh]--;
+
+                if(indegree[neigh]== 0)
+                    q.push(neigh);
+
             }
         }
         return count== numCourses;
