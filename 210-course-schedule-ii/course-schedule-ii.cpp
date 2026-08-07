c894365b3834fdd8f32bcd2b8dc5ce15858e3d40
Niyati <ngupta6_be23@thapar.edu>
2026-08-07 20:17:07 +0530

    Time: 7 ms (32.46%) | Memory: 19 MB (27.15%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,38 @@
-#include<vector>
-#include<queue>
-
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //building graph
         vector<vector<int>> adj(numCourses);
-        for(auto &it: prerequisites){
-            adj[it[1]].push_back(it[0]);
+        vector<int> indegree(numCourses, 0);
+
+        for(auto edge: prerequisites){
+            int u= edge[1];
+            int v= edge[0];
+
+            adj[u].push_back(v);
+            indegree[v]++;
         }
 
-        // indegree array
-        vector<int> indegree(numCourses,0);
-        for(int i=0; i<numCourses; i++){
-            for(int v: adj[i]){
-                indegree[v]++;
-            }
-        }
-
-        // queue for nodes with indegree 0
         queue<int> q;
-        for(int i=0; i<numCourses; i++){
-            if(indegree[i]== 0){
+        for(int i=0; i< numCourses; i++){
+            if(indegree[i]==0)
                 q.push(i);
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto neigh: adj[node]){
+                indegree[neigh]--;
+
+                if(indegree[neigh]==0){
+                    q.push(neigh);
+                }
             }
         }
-        vector<int> topo;
-
-        // kahn's algo
-        while(!q.empty()){
-        int node= q.front();
-        q.pop();
-        topo.push_back(node);
-
-        for(int v: adj[node]){
-            indegree[v]--;
-            if(indegree[v]==0)
-            q.push(v);
-        }
-        }
-        if(topo.size()!= numCourses)
-           return {};
-        return topo;
+        if(ans.size()== numCourses) return ans;
+        return {};
     }
 };
