3a08d9e9522fa7ecde21eb68513518792c8b2321
Niyati <ngupta6_be23@thapar.edu>
2026-09-03 11:13:04 +0530

    Time: 85 ms (66.97%) | Memory: 123.5 MB (44.95%) - LeetSync

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -6,7 +6,7 @@ public:
         long long ans=1;
 
         while(b>0){
-            if(b & 1)
+            if(b% 2)
                 ans= ans*a% mod;
             
             a= a*a% mod;
@@ -18,20 +18,21 @@ public:
     int countGoodArrays(int n, int m, int k) {
         if(k> n-1) return 0;
 
-        // C(n-1, k)
-        long long comb=1;
+        vector<long long> fact(n);
+        fact[0]=1;
 
-        for(int i=1; i<= k; i++){
-            comb= comb*(n-i)% mod;
-            comb= comb* power(i, mod-2)% mod;
+        for(int i=1; i<n; i++){
+            fact[i]= fact[i-1]*i% mod;
         }
 
-        long long ans= comb;
+        long long comb= fact[n-1];
 
-        //first element-> m choices
-        ans= ans*m% mod;
+        comb= comb* power(fact[k], mod-2)% mod;
+        comb= comb* power(fact[n-1-k], mod-2)% mod;
 
-        //diff positions-> m-1 choices
+        long long ans= m;
+
+        ans= ans* comb% mod;
         ans= ans* power(m-1, n-1-k)% mod;
 
         return ans;
