60836dde4a3304395a7000d415ff90c1d24fccb2
Niyati <ngupta6_be23@thapar.edu>
2026-09-12 13:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (98.44%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxIndex=0;
+        int maxReach=0;
 
-        for(int i=0; i<nums.size(); i++){
-            if(i> maxIndex) return false;
-            maxIndex= max(maxIndex, i+nums[i]);
-        }
+        for(int i=0; i< nums.size(); i++){
+            if(i> maxReach) return false;
+
+            maxReach= max(maxReach, i+nums[i]);
+        }    
         return true;
     }
 };
